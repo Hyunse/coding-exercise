0be6628c2d108857f5eb478431dca1e1c18913af
Kim Hyunse <anzmf12@naver.com>
2017-10-06 07:55:56 +0900

    tripleTroubleBest

--- Javascript/codewar/tripleTrouble.js
+++ Javascript/codewar/tripleTrouble.js
@@ -11,7 +11,7 @@
 // tripledouble(666789, 12345667) == 1
 
 // Brst Solution
-function tripledouble(num1, num2) {
+function tripledoubleBest(num1, num2) {
     for (let i = 0; i < 10; i++) {
         if (new RegExp(`${i}{3}`).test(num1) && new RegExp(`${i}{2}`).test(num2)) {
             return 1;
@@ -20,8 +20,8 @@ function tripledouble(num1, num2) {
     return 0;
 }
 
-console.log(tripledouble(451999277, 41177722899)); // 1
-console.log(tripledouble(1222345, 12345)); // 0
-console.log(tripledouble(12345, 12345)); //0
-console.log(tripledouble(666789, 12345667)); //1
-console.log(tripledouble(10560002, 100)); // 1
+console.log(tripledoubleBest(451999277, 41177722899)); // 1
+console.log(tripledoubleBest(1222345, 12345)); // 0
+console.log(tripledoubleBest(12345, 12345)); //0
+console.log(tripledoubleBest(666789, 12345667)); //1
+console.log(tripledoubleBest(10560002, 100)); // 1
