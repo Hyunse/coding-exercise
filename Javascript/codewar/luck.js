e8641dea4308cafad457cfde64a6ff4d683dfd58
Kim Hyunse <anzmf12@naver.com>
2017-10-09 21:49:18 +0900

    luck

---
+++ Javascript/codewar/luck.js
@@ -0,0 +1,16 @@
+// The test fixture I use for this kata is pre-populated.
+
+// It will compare your guess to a random number generated using:
+
+// Math.floor(Math.random() * 100 + 1)
+// You can pass by relying on luck or skill but try not to rely on luck.
+
+// "The power to define the situation is the ultimate power." - Jerry Rubin
+
+// Good luck!
+
+Math.random = function () {
+    return 0.24;
+}
+  
+var guess = 25;
