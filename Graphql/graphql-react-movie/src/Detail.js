1fcf00f4633b43cab191599a13aca7dffd162121
KimHyunse <anzmf12@naver.com>
2018-07-09 22:53:43 -0400

    Detail CSS / Call Detail API ( Movie Detail & Movie Suggestions)

--- Graphql/graphql-react-movie/src/Detail.js
+++ Graphql/graphql-react-movie/src/Detail.js
@@ -1,6 +1,47 @@
 import React from 'react';
-const Detail = () => (
-  <div>Detail</div>
-);
+import { Query } from 'react-apollo';
+import { MOVIE_DETAILS } from './queries';
+import Movie from './Movie';
+import './Detail.css';
+
+const Detail = ({
+  match: {
+    params: { movieId }
+  }
+}) => {
+  return (
+    <Query query={MOVIE_DETAILS} variables={{ movieId }}>
+      {({ loading, data: { movie, suggestions }, error }) => {
+        if (loading) return 'loading...';
+        if (error) return 'error';
+        return (
+          <React.Fragment>
+            <div className="movie-detail-box">
+              <figure className="movie-figure">
+                <img src={movie.medium_cover_image} alt={movie.title} />
+              </figure>
+              <span className="movie-description">
+                <h3>{movie.title}</h3>
+                {movie.description_intro}
+              </span>
+            </div>
+            <div className="movie-container">
+              {suggestions.map((suggestion) => {
+                return (
+                  <Movie
+                    id={suggestion.id}
+                    title={suggestion.title}
+                    img={suggestion.medium_cover_image}
+                    key={suggestion.id}
+                  />
+                );
+              })}
+            </div>
+          </React.Fragment>
+        );
+      }}
+    </Query>
+  );
+};
 
 export default Detail;
--- Graphql/graphql-react-movie/src/queries.js
+++ Graphql/graphql-react-movie/src/queries.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export const HOME_PAGE = gql`
   query {
-    movies(limit:10, rating: 7) {
+    movies(limit: 20, rating: 9) {
       id
       title
       genres
@@ -10,4 +10,24 @@ export const HOME_PAGE = gql`
       medium_cover_image
     }
   }
-`
+`;
+
+export const MOVIE_DETAILS = gql`
+  query getMovieDetails($movieId: Int!) {
+    movie(id: $movieId) {
+      id
+      title
+      genres
+      rating
+      medium_cover_image
+      description_intro
+      language
+    }
+    suggestions(id: $movieId) {
+      id
+      title
+      medium_cover_image
+      rating
+    }
+  }
+`;
