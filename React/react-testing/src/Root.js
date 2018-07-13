dc99bee012bf5c8f4827f7faa3d79f889f9a6859
KimHyunse <anzmf12@naver.com>
2018-07-13 15:50:57 -0400

    Create Async Middleware

--- React/react-testing/src/Root.js
+++ React/react-testing/src/Root.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
+// import reduxPromise from 'redux-promise';
+import async from 'middlewares/async';
 import reducers from './reducers';
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
+  const store = createStore(reducers, initialState, applyMiddleware(async));
 
-  return (
-    <Provider store={store}>
-      {children}
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
---
+++ React/react-testing/src/middlewares/async.js
@@ -0,0 +1,18 @@
+// export default function({ dispatch }) {
+//   return function(next) {
+//     return function() {};
+//   };
+// }
+
+export default ({ dispatch }) => (next) => (action) => {
+  
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+
+  action.payload.then(function(response) {
+    const newAction = { ...action, payload: response };
+
+    dispatch(newAction);
+  });
+};
