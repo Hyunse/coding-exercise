9838852be896d15d6e0b3e9ed9357e2e6583baf0
KimHyunse <anzmf12@naver.com>
2018-07-18 21:47:14 -0400

    Get oAuthGoogle / Set up Passport

--- Server/express-survey/index.js
+++ Server/express-survey/index.js
@@ -1,10 +1,35 @@
 import express from 'express';
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { GOOGLE_API_KEY } from './config/keys';
 
+// Var
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'HelloWorld' });
-});
+// Pssport
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_API_KEY.clientID,
+      clientSecret: GOOGLE_API_KEY.clientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+
+}))
 
 // Dynamic Port Binding
 const PORT = process.env.PORT || 5000;
