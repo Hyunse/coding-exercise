721de41bd6a899dce17b23a2f5fe721a3405dbd0
KimHyunse <anzmf12@naver.com>
2018-07-21 21:53:20 -0400

    Install modules( Redux, React-Router .. ) & Create Index.js

--- Server/express-survey/client/src/index.js
+++ Server/express-survey/client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+
+// Init Store
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
