83226117abdbe25f7245ffcc0f37394f9d040159
KimHyunse <anzmf12@naver.com>
2018-07-23 21:56:06 -0400

    Add Actions & Test redux-thunk

--- Server/express-survey/client/src/index.js
+++ Server/express-survey/client/src/index.js
@@ -3,12 +3,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 import App from './components/App';
 
 // Init Store
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDom.render(
   <Provider store={store}>
