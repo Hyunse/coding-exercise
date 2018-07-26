9affeb08d7f0beac9609b792755ea2310c447e8e
KimHyunse <anzmf12@naver.com>
2018-07-26 17:23:54 -0400

    Add Payment Component

--- Server/express-survey/client/src/components/Header.js
+++ Server/express-survey/client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import Payments from './Payments';
+
 class Header extends Component {
   renderContent() {
     const { auth } = this.props;
@@ -15,11 +17,14 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
+        return [
+          <li>
+            <Payments />
+          </li>,
           <li>
             <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
 
