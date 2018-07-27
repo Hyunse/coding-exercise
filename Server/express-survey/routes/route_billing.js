1517df1333000de987be07707ff631f8baaf4c2d
KimHyunse <anzmf12@naver.com>
2018-07-27 16:20:24 -0400

    Router stripe api

--- Server/express-survey/routes/route_billing.js
+++ Server/express-survey/routes/route_billing.js
@@ -6,7 +6,12 @@ const router = express.Router();
 const stripeApi = stripe(SRIPE_API_KEY.secretKey);
 
 router.post('/api/stripe', (req, res) => {
-  res.send(req.user);
+  stripeApi.charges.create({
+    amount: 500,
+    currency: 'usd',
+    description: '$5 for 5 credits',
+    source: req.body.id
+  });
 });
 
 export default router;
