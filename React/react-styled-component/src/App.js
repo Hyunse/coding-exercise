06e8580a807261f125efe23e97628f8004354532
KimHyunse <anzmf12@naver.com>
2018-10-08 15:51:32 -0400

    keyframe, css, theme, nesting

--- React/react-styled-component/src/App.js
+++ React/react-styled-component/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import styled, { injectGlobal } from 'styled-components';
+import styled, { injectGlobal, keyframes, css, ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 injectGlobal`
   body{
@@ -8,10 +9,31 @@ injectGlobal`
   }
 `;
 
+const Card = styled.div`
+  background-color: blue;
+`
+
+const Form = () => (
+  <Card>
+    <Button>This is Form Button</Button>
+  </Card>
+);
+
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background: #bdc3c7;
+  /* nesting */
+  ${Card} {
+    background-color: white;
+  }
 `;
 
 const Button = styled.button`
@@ -26,21 +48,47 @@ const Button = styled.button`
   &:focus {
     outline: none;
   }
-  background-color: ${(props) => (props.danger ? 'red' : '#2ecc71')};
+  background-color: ${(props) => (props.danger ? 'red' : props.theme.mainColor)};
+  ${(props) => {
+    if (props.danger) {
+      return `animation: ${rotation} ${props.rotationTime}s linear infinite`;
+    }
+  }};
 `;
 
 const Anchor = Button.withComponent('a').extend`
   text-decoration: none;
 `;
 
+const rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  margin-top: 50px;
+  border-radius: 5px;
+  ${awesomeCard}
+`;
+
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Button success>Hi</Button>
-        <Button danger>Danger</Button>
-        <Anchor href="http://www.google.com">Go to Goo</Anchor>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Button success>Hi</Button>
+          <Button danger rotationTime={5}>
+            Danger
+          </Button>
+          <Anchor href="http://www.google.com">Go to Goo</Anchor>
+          <Input placeholder="Hello" />
+          <Form/>
+        </Container>
+      </ThemeProvider>
     );
   }
 }
---
+++ React/react-styled-component/src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  mainColor: '#27ae60',
+  dangerColor: 'red',
+  successColor: '#f1c40f'
+};
+
+export default theme;
