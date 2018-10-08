a86a67050e0acbc4ab5872deed75d28415d51293
KimHyunse <anzmf12@naver.com>
2018-10-08 12:11:42 -0400

    Using styled-component / extends

--- React/react-styled-component/src/App.js
+++ React/react-styled-component/src/App.js
@@ -1,13 +1,46 @@
-import React, { Component, Fragment } from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import styled, { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body{
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background: #bdc3c7;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${(props) => (props.danger ? 'red' : '#2ecc71')};
+`;
+
+const Anchor = Button.withComponent('a').extend`
+  text-decoration: none;
+`;
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <button className="button button--success">Hello</button>
-        <button className="button button--danger">Hello</button>
-      </Fragment>
+      <Container>
+        <Button success>Hi</Button>
+        <Button danger>Danger</Button>
+        <Anchor href="http://www.google.com">Go to Goo</Anchor>
+      </Container>
     );
   }
 }
