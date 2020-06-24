52a1b26e1bbbaf7d5c12f688dd45ddc0beaae1f9
Hyunse Kim <anzmf12@naver.com>
2020-06-24 18:13:12 -0400

    vue + tailwind

---
+++ Css/tailwind-tutorial/vue-tailwind-test/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="flex flex-wrap flex-col content-center">
+    <HelloWorld msg="Test Project with Tailwind" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
+
---
+++ Css/tailwind-tutorial/vue-tailwind-test/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="head" class="text-6xl">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
