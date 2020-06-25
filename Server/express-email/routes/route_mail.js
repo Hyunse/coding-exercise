283d25e8a800957c32cf9fcdcb13cffaee833781
Hyunse Kim <anzmf12@naver.com>
2020-06-25 13:37:08 -0400

    mailgun email template

--- Server/express-email/routes/route_mail.js
+++ Server/express-email/routes/route_mail.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Mailgun = require('mailgun-js');
+const fs = require('fs');
 
 router.post('/mail', (req, res) => {
   const from = process.env.MAIL_FROM || '';
@@ -15,6 +16,39 @@ router.post('/mail', (req, res) => {
     to,
     subject,
     html: content,
+  };
+
+  mailgun.messages().send(data, function (err, body) {
+    if (err) {
+      res.send({
+        success: false,
+        error: err,
+        message: err.message
+      });
+    } else {
+      res
+        .send({
+          success: true,
+          message: 'Sended',
+        })
+        .status(200);
+    }
+  });
+});
+
+router.post('/mail/template', (req, res) => {
+  const from = process.env.MAIL_FROM || '';
+  const to = req.body.to;
+  const subject = req.body.subject;
+  const mailgun = new Mailgun({
+    apiKey: process.env.MAIL_API_KEY,
+    domain: process.env.MAIL_DOMAIN,
+  });
+  const data = {
+    from,
+    to,
+    subject,
+    template: 'test'
   };
 
   mailgun.messages().send(data, function (err, body) {
