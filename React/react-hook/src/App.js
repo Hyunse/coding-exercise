51d5ab5802a1614726e82a7967ea2197d30927d2
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-06-25 23:49:44 -0400

    testing react hook

---
+++ React/react-hook/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function useInput(defaultValue) {
+  const [value, setValue] = useState(defaultValue);
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+
+    setValue(value);
+  };
+
+  return { value, onChange };
+}
+
+function useFetch() {
+  const [payload, setPayload] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const callUrl = async () => {
+    try {
+      const { data } = await axios.get('https://randomuser.me/api/');
+
+      setPayload(data.results[0].picture.large);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    callUrl();
+  }, []);
+
+  return { payload, loading, error };
+}
+
+function App() {
+  const name = useInput('');
+  const { payload, loading, error } = useFetch();
+  return (
+    <div className="App">
+      <h1>React Hooks</h1>
+      <input {...name} placeholder="name?" />
+      <div>
+        {loading && <span>loading your cat</span>}
+        {!loading && error && <span>{error}</span>}
+        {!loading && payload && (
+          <img src={payload} width="150" height="200" alt="random"></img>
+        )}
+      </div>
+      {name.value}
+    </div>
+  );
+}
+
+export default App;
