535940a20343ff341d7f2c3f6ec4fb7939036e94
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-07-18 02:15:01 -0400

    sound visualization

--- Javascript/music/index.js
+++ Javascript/music/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
--- Javascript/music/public/script.js
+++ Javascript/music/public/script.js
@@ -1,9 +1,31 @@
-let audio, ctx, audioSrc, analyser, source, bufferLength, dataArray;
+let audio, ctx, audioSrc, analyser, source, bufferLength, dataArray, drawVisual;
+const WIDTH = 1024;
+const HEIGHT = 350;
+const INTERVAL = 128;
+
+function start() {
+  if (ctx.state === 'suspended') {
+    ctx.resume();
+  }
+  audio.volume = 0.3;
+  audio.play();
+
+  draw();
+}
+
+function stop() {
+  audio.pause();
+}
 
 function init() {
+  initAudio();
+  initCanvas();
+}
+
+function initAudio() {
   // Get Element
   audio = document.getElementById('audio');
-  canvas = document.getElementById('canvas');
+  audio.src = '/test2.mp3';
 
   // Get Conetxt & analyser
   ctx = new AudioContext();
@@ -20,11 +42,31 @@ function init() {
   source.connect(ctx.destination);
 }
 
-function start() {
-  audio.src = './test.mp3';
-  audio.play();
+function initCanvas() {
+  // Get Element & Access Context
+  canvas = document.getElementById('canvas');
+  canvasCtx = canvas.getContext('2d');
 }
 
-function stop() {
-  audio.pause();
+function draw() {
+  drawVisual = requestAnimationFrame(draw);
+  analyser.getByteFrequencyData(dataArray);
+
+  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+  canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+
+  var x =0;
+
+  for (var i = 0; i < 32; i++) {
+    var y = (dataArray[i] - 128) * 2 + 5;
+    if (y <= 1) {
+      y = 2;
+    }
+
+    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+    canvasCtx.fillRect(x, HEIGHT - y, WIDTH / 32 - 2, y);
+    x = x + (WIDTH / 32);
+  }
 }
+
