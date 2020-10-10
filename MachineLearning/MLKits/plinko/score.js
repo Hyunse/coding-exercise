dbcd2cc2d6d48cb876a575c7fdbea8b228194a5c
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-10-09 21:40:42 -0400

    study

---
+++ MachineLearning/MLKits/plinko/score.js
@@ -0,0 +1,39 @@
+const outputs = [];
+const k = 3;
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+}
+
+function runAnalysis() {
+  const bucket = 
+
+  console.log('Your point will drop to Bucket #' + bucket);
+}
+
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+}
+
+function knn(data, point) {
+  return _.chain(data)
+    .map((row) => [distance(row[0], point), row[3]])
+    .sortBy((row) => row[0])
+    .slice(0, k)
+    .countBy((row) => row[1])
+    .toPairs()
+    .sortBy((row) => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+}
